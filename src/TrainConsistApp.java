e21bb01bd3d4c6603f9426e79cc37b75dc3ac943
rohit1911-code <rsarrsar919@gmail.com>
2026-04-07 09:16:46 +0530

    UC10 - Count Total Seats in Train (reduce)

--- src/TrainConsistApp.java
+++ src/TrainConsistApp.java
@@ -23,7 +23,7 @@ public class TrainConsistApp {
     public static void main(String[] args) {
 
         System.out.println("======================================");
-        System.out.println("UC9 - Group Bogies by Type");
+        System.out.println("UC10 - Count Total Seats in Train");
         System.out.println("======================================\n");
 
         List<Bogie> bogies = new ArrayList<>();
@@ -32,25 +32,18 @@ public class TrainConsistApp {
         bogies.add(new Bogie("AC Chair", 56));
         bogies.add(new Bogie("First Class", 24));
         bogies.add(new Bogie("Sleeper", 70));
-        bogies.add(new Bogie("AC Chair", 60));
 
-        System.out.println("All Bogies:");
+        System.out.println("Bogies in Train:");
         for (Bogie b : bogies) {
             System.out.println(b.getName() + " -> " + b.getCapacity());
         }
 
-        Map<String, List<Bogie>> grouped = bogies.stream()
-                .collect(Collectors.groupingBy(Bogie::getName));
+        int total = bogies.stream()
+                .map(Bogie::getCapacity)
+                .reduce(0, Integer::sum);
 
-        System.out.println("\nGrouped Bogies:");
+        System.out.println("\nTotal Seating Capacity of Train: " + total);
 
-        for (Map.Entry<String, List<Bogie>> entry : grouped.entrySet()) {
-            System.out.println("\nBogie Type: " + entry.getKey());
-            for (Bogie b : entry.getValue()) {
-                System.out.println("Capacity -> " + b.getCapacity());
-            }
-        }
-
-        System.out.println("\nUC9 grouping completed...");
+        System.out.println("\nUC10 aggregation completed...");
     }
 }
