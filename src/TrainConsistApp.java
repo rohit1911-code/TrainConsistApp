f6e7d6382e8a614441fc647686b446053562e43c
rohit1911-code <rsarrsar919@gmail.com>
2026-04-07 09:23:19 +0530

    UC13:Performance Comparison

--- src/TrainConsistApp.java
+++ src/TrainConsistApp.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.util.stream.*;
+
 class Bogie {
     private String name;
     private int capacity;
@@ -22,27 +23,42 @@ public class TrainConsistApp {
     public static void main(String[] args) {
 
         System.out.println("======================================");
-        System.out.println("UC10 - Count Total Seats in Train");
+        System.out.println("UC13 - Performance Comparison (Loops vs Streams)");
         System.out.println("======================================\n");
 
         List<Bogie> bogies = new ArrayList<>();
 
-        bogies.add(new Bogie("Sleeper", 72));
-        bogies.add(new Bogie("AC Chair", 56));
-        bogies.add(new Bogie("First Class", 24));
-        bogies.add(new Bogie("Sleeper", 70));
-
-        System.out.println("Bogies in Train:");
-        for (Bogie b : bogies) {
-            System.out.println(b.getName() + " -> " + b.getCapacity());
+        for (int i = 0; i < 100000; i++) {
+            bogies.add(new Bogie("Sleeper", 72));
+            bogies.add(new Bogie("AC Chair", 56));
+            bogies.add(new Bogie("First Class", 24));
         }
 
-        int total = bogies.stream()
-                .map(Bogie::getCapacity)
-                .reduce(0, Integer::sum);
+        long startLoop = System.nanoTime();
 
-        System.out.println("\nTotal Seating Capacity of Train: " + total);
+        List<Bogie> loopResult = new ArrayList<>();
+        for (Bogie b : bogies) {
+            if (b.getCapacity() > 60) {
+                loopResult.add(b);
+            }
+        }
 
-        System.out.println("\nUC10 aggregation completed...");
+        long endLoop = System.nanoTime();
+
+        long startStream = System.nanoTime();
+
+        List<Bogie> streamResult = bogies.stream()
+                .filter(b -> b.getCapacity() > 60)
+                .collect(Collectors.toList());
+
+        long endStream = System.nanoTime();
+
+        long loopTime = endLoop - startLoop;
+        long streamTime = endStream - startStream;
+
+        System.out.println("Loop Execution Time (ns): " + loopTime);
+        System.out.println("Stream Execution Time (ns): " + streamTime);
+
+        System.out.println("\nUC13 performance benchmarking completed...");
     }
 }
